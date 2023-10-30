d17078cb31d76a259b790e67eb77bef5fa21d11f
devesh143 <104589801+devesh143@users.noreply.github.com>
2023-10-30 12:09:02 +0530

    Add files via upload

---
+++ Untitled50.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Notebook Title
+
+
+# In[9]:
+
+
+#Introduction
+# Welcome to this notebook where we explore various aspects of data science!
+
+
+# In[3]:
+
+
+## Data Science Languages
+#- Python
+#- R
+#- Julia
+
+
+# In[4]:
+
+
+## Data Science Libraries
+#- Pandas
+#- NumPy
+#- Scikit-learn
+
+
+# In[5]:
+
+
+## Data Science Tools
+
+#| Tool         | Description                           |
+#|--------------|---------------------------------------|
+#| Jupyter      | Interactive computing environment     |
+#| TensorFlow   | Machine learning library by Google     |
+#| Matplotlib   | Plotting library for Python            |
+
+
+# In[6]:
+
+
+## Arithmetic Expression Examples
+#Let's explore some examples of arithmetic expressions in this section.
+
+
+# In[7]:
+
+
+# Example of multiplying and adding numbers
+# a = 5
+# b = 10
+# result = a * b + 20
+# result
+
+
+# In[8]:
+
+
+## Objectives
+#- Explore data science languages and libraries
+#- Understand arithmetic expressions
+#- Convert units of time
+
+
+# In[ ]:
+
+
+## Author
+# Anupam Jha
+
